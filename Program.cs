bdf8f9991c451e9700808c754775c9afb94ee4a9
Bakr_Sayed <bakrsayed864@gmail.com>
2024-08-28 21:16:46 +0300

    solve problem You are given the heads of two sorted linked lists list1 and list2.Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.Return the head of the merged linked list.

--- Program.cs
+++ Program.cs
@@ -45,7 +45,7 @@ namespace test
         /// Given the head of a linked list, remove the nth node from the end of the list and return its head.
         /// </summary>
         /// 19. Remove Nth Node From End of List
-        /// <param name="args"></param>
+        /// <param></param>
         public static ListNode RemoveNthFromEnd(ListNode head, int n)
         {
             int listCount = 0;
@@ -67,24 +67,70 @@ namespace test
             return dummy.next;
         }
 
+        /// <summary>
+        /// You are given the heads of two sorted linked lists list1 and list2.
+        ///Merge the two lists into one sorted list.The list should be made by splicing together the nodes of the first two lists.
+        ///Return the head of the merged linked list.
+        /// </summary>
+        /// 21. Merge Two Sorted Lists
+        /// <returns></returns>
+        public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode dummy = new ListNode(0);
+            ListNode dummyNode = dummy;
+            while (list1 != null || list2 != null)
+            {
+                if (list1 == null)
+                {
+                    dummyNode.next = list2;
+                    dummyNode = dummyNode.next;
+                    list2 = list2.next;
+                }
+                else if(list2 == null)
+                {
+                    dummyNode.next = list1;
+                    dummyNode = dummyNode.next;
+                    list1 = list1.next;
+                }
+                else if (list1.val < list2.val)
+                {
+                    dummyNode.next = list1;
+                    dummyNode = dummyNode.next;
+                    list1 = list1.next;
+                }
+                else if (list2.val < list1.val)
+                {
+                    dummyNode.next = list2;
+                    dummyNode = dummyNode.next;
+                    list2 = list2.next;
+                }
+                else
+                {
+                    dummyNode.next = list1;
+                    dummyNode = dummyNode.next;
+                    list1 = list1.next;
+                    dummyNode.next = list2;
+                    dummyNode = dummyNode.next;
+                    list2 = list2.next;
+                }
+            }
+            return dummy.next;
+        }
         static void Main(string[] args)
         {
-            ListNode head  = new ListNode(2);
-            ListNode next0 = new ListNode(2);
+            ListNode list1  = new ListNode(1);
             ListNode next1 = new ListNode(2);
-            ListNode next2 = new ListNode(2);
-           
-            //ListNode next4 = new ListNode(2);
-            //ListNode next5 = new ListNode(2);
+            ListNode next2 = new ListNode(4);
+            ListNode list2 = new ListNode(1);
 
-            head.next = next0;
-            next0.next = next1;
+            ListNode next4 = new ListNode(3);
+            ListNode next5 = new ListNode(4);
+
+            list1.next = next1;
             next1.next = next2;
-            //
-           
-            //next3.next = next4;
-            //next4.next = next5;
-            RemoveElements(head, 2);
+            list2.next = next4;
+            next4.next = next5;
+            MergeTwoLists(list1,list2);
         }
     }
 }
