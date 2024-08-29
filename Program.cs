b0e1162ba8247062a82803b17cabf30cd4fba940
Bakr_Sayed <bakrsayed864@gmail.com>
2024-08-29 21:33:54 +0300

    solve problem 1. Two Sum

--- Program.cs
+++ Program.cs
@@ -249,6 +249,30 @@ namespace test
         }
 
         /// <summary>
+        /// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target
+        /// You may assume that each input would have exactly one solution, and you may not use the same element twice.You can return the answer in any order.
+        /// </summary>
+        /// 1. Two Sum
+        /// <returns></returns>
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int[] outt = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        outt[0] = i;
+                        outt[1] = j;
+                        return outt;
+                    }
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
         /// Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus the sum of all keys greater than the original key in BST.
         /// As a reminder, a binary search tree is a tree that satisfies these constraints: The left subtree of a node contains only nodes with keys less than the node's key.
         /// The right subtree of a node contains only nodes with keys greater than the node's key. Both the left and right subtrees must also be binary search trees.
