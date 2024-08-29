36d597c58c2ed46f812baf2e35a8135ffa080db0
Bakr_Sayed <bakrsayed864@gmail.com>
2024-08-29 21:22:09 +0300

    solve problem 1038. Binary Search Tree to Greater Sum Tree

--- Program.cs
+++ Program.cs
@@ -13,6 +13,18 @@ namespace test
            this.next = next;
        }
     }
+    public class TreeNode
+    {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+       {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+       }
+  }
     internal class Program
     {
         /// <summary>
@@ -159,6 +171,38 @@ namespace test
 
             return collisions;
         }
+
+        /// <summary>
+        /// Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus the sum of all keys greater than the original key in BST.
+        /// As a reminder, a binary search tree is a tree that satisfies these constraints: The left subtree of a node contains only nodes with keys less than the node's key.
+        /// The right subtree of a node contains only nodes with keys greater than the node's key. Both the left and right subtrees must also be binary search trees.
+        /// </summary>
+        /// 1038. Binary Search Tree to Greater Sum Tree
+        /// <returns></returns>
+        public static TreeNode BstToGst(TreeNode root)
+        {
+            int sum = 0;
+            TransformTree(root, ref sum);
+            return root;
+        }
+        private static void TransformTree(TreeNode node, ref int sum)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            // Traverse the right subtree first
+            TransformTree(node.right, ref sum);
+
+            // Update the current node's value with the cumulative sum
+            sum += node.val;
+            node.val = sum;
+
+            // Traverse the left subtree
+            TransformTree(node.left, ref sum);
+        }
+
         /// <summary>
         /// You are given an m x n integer matrix points (0-indexed). Starting with 0 points, you want to maximize the number of points you can get from the matrix
         /// To gain points, you must pick one cell in each row.Picking the cell at coordinates (r, c) will add points[r][c] to your score.
