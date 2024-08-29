749b63f218268f829b1d9bf809edbe39e97f9c10
Bakr_Sayed <bakrsayed864@gmail.com>
2024-08-29 21:18:37 +0300

    solve problem 2211. Count Collisions on a Road

--- Program.cs
+++ Program.cs
@@ -113,6 +113,53 @@ namespace test
         }
 
         /// <summary>
+        /// There are n cars on an infinitely long road. The cars are numbered from 0 to n - 1 from left to right and each car is present at a unique point.
+        /// You are given a 0-indexed string directions of length n.directions[i] can be either 'L', 'R', or 'S' denoting whether the ith car is moving towards the left, towards the right, or staying at its current point respectively.Each moving car has the same speed.
+        /// The number of collisions can be calculated as follows:
+        /// When two cars moving in opposite directions collide with each other, the number of collisions increases by 2.
+        /// When a moving car collides with a stationary car, the number of collisions increases by 1
+        /// After a collision, the cars involved can no longer move and will stay at the point where they collided. Other than that, cars cannot change their state or direction of motion.
+        /// Return the total number of collisions that will happen on the road.
+        /// </summary>
+        /// 2211. Count Collisions on a Road
+        /// <returns></returns>
+        public static int CountCollisions(string directions)
+        {
+            int collisions = 0;
+            char[] cars = directions.ToCharArray();
+
+            // Process collisions
+            for (int i = 0; i < cars.Length; i++)
+            {
+                // Check for left moving car colliding with a stationary or right moving car
+                if (cars[i] == 'L' && i > 0 && (cars[i - 1] == 'R' || cars[i - 1] == 'S'))
+                {
+                    collisions += (cars[i - 1] == 'R') ? 2 : 1;
+                    cars[i] = 'S';
+                    cars[i - 1] = 'S';
+                }
+                // Check for right moving car colliding with a stationary car
+                else if (cars[i] == 'R' && i < cars.Length - 1 && cars[i + 1] == 'S')
+                {
+                    collisions += 1;
+                    cars[i] = 'S';
+                }
+            }
+
+            // Process remaining right moving cars that collide with stationary cars
+            for (int i = cars.Length - 1; i >= 0; i--)
+            {
+                if (cars[i] == 'R' && i < cars.Length - 1 && (cars[i + 1] == 'L' || cars[i + 1] == 'S'))
+                {
+                    collisions += (cars[i + 1] == 'L') ? 2 : 1;
+                    cars[i] = 'S';
+                    cars[i + 1] = 'S';
+                }
+            }
+
+            return collisions;
+        }
+        /// <summary>
         /// You are given an m x n integer matrix points (0-indexed). Starting with 0 points, you want to maximize the number of points you can get from the matrix
         /// To gain points, you must pick one cell in each row.Picking the cell at coordinates (r, c) will add points[r][c] to your score.
         /// However, you will lose points if you pick a cell too far from the cell that you picked in the previous row.For every two adjacent rows r and r + 1 (where 0 <= r<m - 1), picking cells at coordinates(r, c1) and(r + 1, c2) will subtract abs(c1 - c2) from your score.
